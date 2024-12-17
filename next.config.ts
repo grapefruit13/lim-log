69f1bbfc768a5cc9906d2ae6961fb5040c0e2f28
grapefruit <liam.heo11@gmail.com>
2024-12-17 16:40:39 +0900

    chore: next config image remotepatterns 허용

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   transpilePackages: ['next-mdx-remote'],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https', // 모든 HTTPS 프로토콜
+        hostname: '**', // 모든 도메인 허용
+      },
+      {
+        protocol: 'http', // HTTP 프로토콜도 허용
+        hostname: '**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
