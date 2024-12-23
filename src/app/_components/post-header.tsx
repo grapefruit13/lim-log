f63a386a3dfb78ba88ce444a0c1dd43d0ec128fe
grapefruit <liam.heo11@gmail.com>
2024-12-23 23:55:59 +0900

    style: update style of PostHeader

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -13,25 +13,30 @@ type Props = {
 
 export function PostHeader({ title, coverImage, date, author }: Props) {
   return (
-    // <div className='flex flex-col justify-center items-center'>
     <>
       <PostTitle>{title}</PostTitle>
-      <div className='hidden md:block md:mb-12'>
+      <div className='hidden md:mb-8 md:flex md:gap-3 md:items-center'>
         <Avatar name={author.name} picture={author.picture} />
+        <div className='text-lg dark:text-gray-300 flex gap-3'>
+          <span>·</span>
+          <DateFormatter dateString={date} />
+        </div>
       </div>
-      <div className='mb-8 md:mb-16 sm:mx-0'>
-        <CoverImage title={title} src={coverImage} />
+      <div className='mb-16 md:mb-16 sm:mx-0'>
+        <CoverImage
+          title={title}
+          src={coverImage}
+          imageClassName='max-w-[600px] mx-auto'
+        />
       </div>
-      {/* <div className='max-w-2xl mx-auto'> */}
       <div className='max-w-full mx-auto'>
         <div className='block md:hidden mb-6'>
           <Avatar name={author.name} picture={author.picture} />
         </div>
-        <div className='mb-6 text-lg'>
+        <div className='md:hidden mb-6 text-lg'>
           <DateFormatter dateString={date} />
         </div>
       </div>
     </>
-    //  </div>
   );
 }
