c3bfd01e7bec676bd4aa447b8a544dddbb4d5a22
grapefruit <liam.heo11@gmail.com>
2024-12-31 17:01:23 +0900

    style: modify style of more-stories

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -10,9 +10,9 @@ export function MoreStories({ posts }: Props) {
   return (
     <section>
       <h2 className='mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight'>
-        More Stories
+        More stories
       </h2>
-      <div className='grid grid-cols-1 md:grid-cols-2 md:gap-x-12 lg:gap-x-16 gap-y-12 md:gap-y-16 mb-32'>
+      <div className='flex flex-col gap-2 mb-32'>
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
