5e1e018dd27a07763710303ef3e24a1072e0a48d
grapefruit <liam.heo11@gmail.com>
2025-01-08 17:05:03 +0900

    fix: modify useThemeStore

--- src/stores/useThemeStore.ts
+++ src/stores/useThemeStore.ts
@@ -1,6 +1,4 @@
-import { STORAGE_KEY } from '@/app/_components/theme-switcher';
 import { create } from 'zustand';
-
 export type ColorSchemePreference = 'system' | 'dark' | 'light';
 
 type ThemeState = {
@@ -8,20 +6,8 @@ type ThemeState = {
   setMode: (mode: ColorSchemePreference) => void;
 };
 
-export const useThemeStore = create<ThemeState>((set) => ({
-  mode: 'system', // 기본값 설정
+export const useThemeState = create<ThemeState>((set) => ({
+  // default mode is system
+  mode: 'system',
   setMode: (mode) => set({ mode }),
 }));
-
-// 클라이언트에서만 상태를 설정하도록 useEffect 추가
-export const initializeTheme = () => {
-  if (typeof window !== 'undefined') {
-    const savedMode = localStorage.getItem(STORAGE_KEY) ?? 'system';
-    const systemMode = window.matchMedia('(prefers-color-scheme: dark)').matches
-      ? 'dark'
-      : 'light';
-    const resolvedMode = savedMode === 'system' ? systemMode : savedMode;
-
-    useThemeStore.getState().setMode(resolvedMode as ColorSchemePreference); // Zustand 상태 업데이트
-  }
-};
