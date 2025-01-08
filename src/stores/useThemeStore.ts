7debf385081dbd5b007f1515fee3eb29f63a2149
grapefruit <liam.heo11@gmail.com>
2025-01-08 17:06:06 +0900

    fix: fix typo of useThemeStore

--- src/stores/useThemeStore.ts
+++ src/stores/useThemeStore.ts
@@ -6,7 +6,7 @@ type ThemeState = {
   setMode: (mode: ColorSchemePreference) => void;
 };
 
-export const useThemeState = create<ThemeState>((set) => ({
+export const useThemeStore = create<ThemeState>((set) => ({
   // default mode is system
   mode: 'system',
   setMode: (mode) => set({ mode }),
